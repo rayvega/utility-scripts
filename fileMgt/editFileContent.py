601bcf568ab2b459e61e0eadc84ecb71eece678b
Ray Vega <raymond.vega.0@gmail.com>
2020-10-01 22:30:11 -0700

    add allowing multiple text changes to file content in 'edit file: content' script

--- fileMgt/editFileContent.py
+++ fileMgt/editFileContent.py
@@ -1,12 +1,11 @@
-def update_text(original_text, line_number, text_to_insert):
+def update_text(original_text, text_changes):
     new_lines = []
-
-    text_to_insert_lines = text_to_insert.splitlines()
 
     for count, line in enumerate(original_text.splitlines()):
         current_line_number = count + 1
 
-        if current_line_number == line_number:
+        if current_line_number in text_changes:
+            text_to_insert_lines = text_changes[current_line_number].splitlines()
             new_lines.extend(text_to_insert_lines)
 
         new_lines.append(line)
